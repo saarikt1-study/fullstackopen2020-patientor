78aabb67ac5696a85894d8f178c86be5b6180714
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-06-09 10:12:30 +0200

    Finish 9.10

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
 const app = express();
 app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
+app.use('/api/diagnoses', diagnoseRouter);
 
 const PORT = 3001;
 
